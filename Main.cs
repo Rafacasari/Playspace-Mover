e4320236976fb8b6f2d7c7f3c798793d946a47e7
Rafa <rafagarciagamer@gmail.com>
2020-12-28 01:38:57 -0300

    1.1.1

--- Main.cs
+++ Main.cs
@@ -2,6 +2,9 @@
 using MelonLoader;
 using System.Collections;
 using System.Collections.Generic;
+//using System.Linq;
+//using System;
+//using UIExpansionKit.API;
 using UnityEngine;
 #endregion
 
@@ -9,11 +12,11 @@ namespace PlayspaceMover
 {
     public static class ModInfo
     {
-        public const string Name = "PlayspaceMover";
-        public const string Description = "A SteamVR's Playspace clone for Oculus Users";
+        public const string Name = "OculusPlayspaceMover";
+        public const string Description = "A SteamVR's Playspace clone for VRChat from Oculus Store";
         public const string Author = "Rafa";
         public const string Company = "RBX";
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
         public const string DownloadLink = null;
     }
 
@@ -23,18 +26,45 @@ namespace PlayspaceMover
         private bool Enabled = true;
         private float Strength = 1f;
         private float DoubleClickTime = 0.25f;
+        private bool DisableDoubleClick = false;
+        private bool DisableLeftHand = false;
+        private bool DisableRightHand = false;
         #endregion
 
         private readonly string Category = "PlayspaceMover";
         public override void OnApplicationStart()
         {
-            MelonPrefs.RegisterCategory(Category, "Playspace Mover");
+            MelonPrefs.RegisterCategory(Category, "Oculus Playspace Mover");
             MelonPrefs.RegisterBool(Category, nameof(Enabled), Enabled, "Enabled");
             MelonPrefs.RegisterFloat(Category, nameof(Strength), Strength, "Strength");
             MelonPrefs.RegisterFloat(Category, nameof(DoubleClickTime), DoubleClickTime, "Double Click Time");
+            MelonPrefs.RegisterBool(Category, nameof(DisableDoubleClick), DisableDoubleClick, "Disable Double Click");
+            MelonPrefs.RegisterBool(Category, nameof(DisableLeftHand), DisableLeftHand, "Disable Left Hand");
+            MelonPrefs.RegisterBool(Category, nameof(DisableRightHand), DisableRightHand, "Disable Right Hand");
+
             ApplySettings();
 
             MelonCoroutines.Start(WaitInitialization());
+            
+            //if (MelonHandler.Mods.Any(x => x.Info.Name == "UI Expansion Kit"))
+            //{
+            //    BindManager.Initialize();
+
+            //    var playspaceSettings = ExpansionKitApi.CreateCustomFullMenuPopup(LayoutDescription.WideSlimList);
+            //    playspaceSettings.AddSimpleButton("Left Hand", new Action(() => BindManager.Show("Left Hand Spacedrag", new Action<KeyCode>(key =>
+            //        {
+
+            //        }), null)
+            //    ));
+
+            //    playspaceSettings.AddSimpleButton("Right Hand", new Action(() => 
+            //        BindManager.Show("Right Hand Spacedrag", new Action<KeyCode>(key => {
+
+            //        }), null)
+            //    ));
+
+            //    ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Oculus Playspace", new Action(() => playspaceSettings.Show()));
+            //} 
         }
 
         private void ApplySettings()
@@ -42,6 +72,9 @@ namespace PlayspaceMover
             Enabled = MelonPrefs.GetBool(Category, nameof(Enabled));
             Strength = MelonPrefs.GetFloat(Category, nameof(Strength));
             DoubleClickTime = MelonPrefs.GetFloat(Category, nameof(DoubleClickTime));
+            DisableDoubleClick = MelonPrefs.GetBool(Category, nameof(DisableDoubleClick));
+            DisableLeftHand = MelonPrefs.GetBool(Category, nameof(DisableLeftHand));
+            DisableRightHand = MelonPrefs.GetBool(Category, nameof(DisableRightHand));
         }
 
         public override void OnModSettingsApplied() => ApplySettings();
@@ -66,13 +99,11 @@ namespace PlayspaceMover
             yield break;
         }
 
-
-
         public override void OnUpdate()
         {
             if (!Enabled || Camera == null) return;
 
-            if (HasDoubleClicked(OVRInput.Button.Three, DoubleClickTime) || HasDoubleClicked(OVRInput.Button.One, DoubleClickTime))
+            if (!DisableDoubleClick && (HasDoubleClicked(OVRInput.Button.Three, DoubleClickTime) || HasDoubleClicked(OVRInput.Button.One, DoubleClickTime)))
             {
                 Camera.cameraLiftTransform.localPosition = StartPosition;
                 return;
@@ -86,7 +117,7 @@ namespace PlayspaceMover
             bool leftTrigger = OVRInput.Get(OVRInput.Button.Three, OVRInput.Controller.Touch);
             bool rightTrigger = OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.Touch);
 
-            if (leftTrigger)
+            if (leftTrigger && !DisableLeftHand)
             {
                 Vector3 currentOffset = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
                 Vector3 calculatedOffset = (currentOffset - startingOffset) * -Strength;
@@ -94,7 +125,7 @@ namespace PlayspaceMover
                 Camera.cameraLiftTransform.localPosition += calculatedOffset;
             }
 
-            if (rightTrigger)
+            if (rightTrigger && !DisableRightHand)
             {
                 Vector3 currentOffset = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
                 Vector3 calculatedOffset = (currentOffset - startingOffset) * -Strength;
